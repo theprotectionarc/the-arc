37cd3f31eccf732f35926712de0b7b6093e4eeae
theprotectionarc <133853388+theprotectionarc@users.noreply.github.com>
2023-06-01 00:48:02 +0800

    Delete QRCODE.js

--- QRCODE.js
+++
@@ -1,8 +0,0 @@
-function resetQRCode() {
-    document.getElementById("qrcode").innerHTML = "";
-    document.getElementById("text-input").value = "";
-    document.getElementById("number-input").value = "";
-    document.getElementById("email-input").value = "";
-    document.getElementById("address-input").value = "";
-    qrcode = null;
-  }
